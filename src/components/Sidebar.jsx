e64812bd112231d24f0cc0ad0dc705c3967c6cc2
japanessential <lama.nawraj00@gmail.com>
2025-02-23 23:22:16 +0900

    api is applied to fetch videos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,7 @@
 import {Stack} from '@mui/material';
 import { categories } from '../utils/constants';
 
-const selectedCategory ='New';
- const Sidebar = () => {
+ const Sidebar = ({selectedCategory,setSelectedCategory}) => {
   return (
     <Stack direction="row"
      sx={{
@@ -12,6 +11,7 @@ const selectedCategory ='New';
     }}>
             {categories.map((category)=>(
                 <button className='category-btn'
+                onClick={()=>setSelectedCategory (category.name)}
                     style={{
                         background: category.name === selectedCategory && '#FC1503',
                         color:'white'
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+
+const options = {
+   
+    headers:{
+        'Content-Type':'application/json',
+        
+            'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY, // Ensure this is correctly set in your .env file
+            'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+          
+    },
+}
+
+export const fetchFromAPI= async (url)=>{
+    try{
+        const {data}=await axios.get(`${BASE_URL}/${url}`,options)
+        return data;
+        
+    }catch(error){
+        console.error('Error fetching data from API',error);
+        throw error;
+    }
+}
