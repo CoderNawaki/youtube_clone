cf220e700f9b15ce897bc12b245d6f70bf25d343
japanessential <lama.nawraj00@gmail.com>
2025-10-14 23:32:56 +0900

    edit the searchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,6 +6,20 @@ import {Search} from '@mui/icons-material';
 
 
 const SearchBar = () => {
+
+  const [searchTerm,setSearchTerm]=useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(searchTerm){
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  }
+
   return (
     <Paper component="form" onSubmit={()=>{}} 
     sx={{
@@ -14,7 +28,7 @@ const SearchBar = () => {
         pl:2,boxShadow:'none',
         mr:{sm:5}
     }}>
-        <input className='search-bar' placeholder="Search..." value="" onChange={()=>{}} />
+        <input className='search-bar' placeholder="Search..." value="searchTerm" onChange={(e)=>setSearchTerm(e.target.value)} />
         <IconButton type='Submit' sx={{p:'10px',color:'red'}}>
             <Search />
         </IconButton>
