70961a512cb0714bc9730aeaefbcb8999fafd191
japanessential <lama.nawraj00@gmail.com>
2026-07-06 18:49:53 +0900

    Phase 4 performance: code splitting, memo, keys, remove unused font

--- src/components/video/Videos.jsx
+++ src/components/video/Videos.jsx
@@ -20,8 +20,8 @@ const Videos = ({ videos, direction }) => {
       justifyContent="start"
       gap={2}
     >
-      {videos.map((item, idx) => (
-        <Box key={idx}>
+      {videos.map((item) => (
+        <Box key={item.id.videoId || item.id.channelId}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
